fc9461658dfd4e99613c42a1b5a4e247784012ff
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-08-24 19:24:38 +0200

    modified strjoin

--- src/string/ft_strjoin.c
+++ src/string/ft_strjoin.c
@@ -6,45 +6,27 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 17:35:48 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/07/11 16:46:33 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/08/24 19:24:27 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
-
-static void	ft_join1(char **joined, const char **s1, int *i, int *j)
-{
-	*j = 0;
-	while ((*s1)[*i])
-	{
-		(*joined)[*i] = (*s1)[*j];
-		(*i)++;
-		(*j)++;
-	}
-}
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-	int		i;
-	int		j;
 	char	*joined;
+	int		joined_len;
 
-	if (s1 == NULL || s2 == NULL)
+	joined_len = ft_strlen(s1) + ft_strlen(s2);
+	if (joined_len == 0)
 		return (NULL);
-	i = 0;
-	joined = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	joined = malloc(joined_len + 1 * sizeof(*joined));
 	if (joined == NULL)
 		return (NULL);
-	j = 0;
-	ft_join1(&joined, &s1, &i, &j);
-	j = 0;
-	while (s2[j])
-	{
-		joined[i] = s2[j];
-		j++;
-		i++;
-	}
-	joined[i] = 0;
-	return (joined);
+	while (s1 && *s1)
+		*joined++ = *s1++;
+	while (s2 && *s2)
+		*joined++ = *s2++;
+	*joined = '\0';
+	return (joined - joined_len);
 }
