81c32fd86fe1f58bbfed2d32fee4b04274502781
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-09-09 10:41:23 +0200

    simplyfy putptr function

--- src/io/ft_putptr.c
+++ src/io/ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 16:03:18 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/08/15 17:46:58 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/09/09 10:40:15 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,5 @@
 
 int	ft_putptr(void *ptr)
 {
-	int		printed_chars;
-
-	printed_chars = ft_putstr("0x");
-	printed_chars += ft_putlhex((unsigned long long)ptr);
-	return (printed_chars);
+	return (ft_putstr("0x") + ft_putlhex((unsigned long long)ptr));
 }
