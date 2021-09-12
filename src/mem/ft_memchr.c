28e0eb986e0ec92b060c6186c092a5dcc9ee3b92
Toni Schmitt <tonischmitt@online.de>
2021-09-12 17:17:52 +0200

    now compares to unsigned char instead of int

--- src/mem/ft_memchr.c
+++ src/mem/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 19:50:39 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/07/14 12:34:39 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/09/12 16:57:45 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	buf = s;
 	while (n > 0)
 	{
-		if (*buf == c)
+		if (*buf == (unsigned char)c)
 			return ((void *)buf);
 		buf++;
 		n--;
